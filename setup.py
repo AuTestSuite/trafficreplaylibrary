555350104da2d9e4f81c1759e91d21f24fea0e50
Jesse Zhang <xzhan121@illinois.edu>
2018-12-03 16:47:30 -0600

    Fix bad Apache license classifier and update gitignore

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(name="trlib",
       classifiers=[
           'Environment :: Console',
           'Intended Audience :: Developers',
-          'License :: OSI Approved :: Apache License 2.0',
+          'License :: OSI Approved :: Apache Software License',
           'Operating System :: OS Independent',
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.5',
